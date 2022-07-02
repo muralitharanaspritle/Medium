7ef8fd8c631e49d0065b44b9bc2577f5724dee55
muralitharanaspritle <muralitharan.a@spritle.com>
2022-07-02 14:27:58 +0530

    first medium commit

---
+++ Navigators/Navigator.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../Screens/Home/Home";
+import Login from "../Screens/Login/Login";
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
